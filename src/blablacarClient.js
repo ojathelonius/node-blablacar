ee481635cd281682a2ec103210027724fd15c547
ojathelonius <louis.johanet@epfedu.fr>
2017-10-27 14:09:32 +0200

    * Updated functions to stay closer to the Blablacar API

--- src/blablacarClient.js
+++ src/blablacarClient.js
@@ -1,27 +1,26 @@
 import request from "request-promise";
 
 export default class BlablacarClient {
-  constructor(apiKey, locale = "en_US", format = "json") {
+  constructor(apiKey) {
     var httpOptions;
     if (!apiKey) {
       throw new Error("API key required");
     }
-    this.locale = locale;
     this.httpOptions = {
       qs: {
         key: apiKey
       },
       headers: {
         "User-Agent": "node-blablacar"
-      },
-      json: format === "json"
+      }
     };
   }
 
-  getTripDetails(tripId) {
+  getTripDetails(tripDetails = {}) {
     return request({
       ...this.httpOptions,
-      uri: `https://public-api.blablacar.com/api/v2/trips/${tripId}`
+      json: tripDetails._format === "json",
+      uri: `https://public-api.blablacar.com/api/v2/trips/${tripDetails.Trip_ID}?locale=${tripDetails.locale}&_format=${tripDetails.format}`
     });
   }
 
@@ -29,6 +28,7 @@ export default class BlablacarClient {
     let params = this.concatUrl(trip);
     return request({
       ...this.httpOptions,
+      json: trip._format === "json",
       uri: `https://public-api.blablacar.com/api/v2/trips?${params}`
     });
   }
